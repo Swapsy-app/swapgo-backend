402dab17dc1616999eda58dc3c6571712daff952
Gaurav Dixit <dgaurav300@gmail.com>
2024-12-10 18:29:07 +0530

    Add app.js File

---
+++ app.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express=require("express");
+
+
+const app=express();
+app.use(express.json());
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port http://localhost:${process.env.PORT}`);
+})
